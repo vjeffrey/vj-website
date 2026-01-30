17ed6e85c6a8d204bc9653968d592d1f6a37bfbc
vj <victoria.daphnee.jeffrey@gmail.com>
2026-01-30 13:28:49 -0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/vj-website',
+  assetPrefix: '/vj-website/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
